494ea6421219b10c4f2c449c0584f5512ce894f0
Alex Mirrington <alexmirrington@gmail.com>
2020-07-07 11:31:14 +1000

    Add eslint and prettier

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const server = express();
-const port = 8080
+const port = 8080;
 
-server.set('port', process.env.PORT || port);
-server.use('/', express.static(path.join(__dirname, 'dist')));
-server.listen(
-    port,
-    () => console.log(
-        `Server running on port ${server.get("port")} in ${server.get("env")} mode.`
-    )
+server.set("port", process.env.PORT || port);
+server.use("/", express.static(path.join(__dirname, "dist")));
+server.listen(port, () =>
+  console.log(
+    `Server running on port ${server.get("port")} in ${server.get(
+      "env",
+    )} mode.`,
+  ),
 );
