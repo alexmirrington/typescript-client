2935ed0b7aa6f6f718090c6fe84ba56341b8d110
Alex Mirrington <alexmirrington@gmail.com>
2020-07-08 13:24:09 +1000

    Modify tests

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
 module.exports = {
   preset: "ts-jest",
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  transform: {
-    // Regex selects .tsx+ files to be transpiled to .jsx+ before running.
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
   globals: {
     // Point ts-jest to jsconfig for transpilation.
     "ts-jest": {
       tsConfig: "tsconfig.json",
     },
   },
+  transform: {
+    // Regex selects .tsx+ files to be transpiled to .jsx? before running.
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testMatch: [
+    // Path query that selects test files to run.
+    "<rootDir>/tests/**/*.test.(ts|tsx|js|jsx)",
+    "<rootDir>/**/__tests__/*.test.(ts|tsx|js|jsx)",
+  ],
 };
--- src/components/greeting/__tests__/Greeting.test.tsx
+++ src/components/greeting/__tests__/Greeting.test.tsx
@@ -4,14 +4,16 @@ import "@testing-library/jest-dom/extend-expect";
 
 import Greeting from "../Greeting";
 
-test("Renders a greeting", () => {
-  const compiler = "TypeScript";
-  const framework = "React";
+describe("Greeting Component", () => {
+  it("renders a greeting", () => {
+    const compiler = "TypeScript";
+    const framework = "React";
 
-  const { getByText } = render(
-    <Greeting compiler={compiler} framework={framework} />,
-  );
-  expect(
-    getByText(`Hello from ${compiler} and ${framework}!`),
-  ).toBeInTheDocument();
+    const { getByText } = render(
+      <Greeting compiler={compiler} framework={framework} />,
+    );
+    expect(
+      getByText(`Hello from ${compiler} and ${framework}!`),
+    ).toBeInTheDocument();
+  });
 });
