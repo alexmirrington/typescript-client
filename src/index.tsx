db8f18b234584370da115bf01d91755c8eaed44a
Alex Mirrington <alexmirrington@gmail.com>
2021-02-17 16:09:26 +1100

    Remove autoformat on commit It is better to stop the commit instead

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Greeting from "./components/greeting";
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.render(<React.StrictMode>
     <Greeting compiler="TypeScript" framework="React" />
   </React.StrictMode>,
   document.getElementById("content"),
